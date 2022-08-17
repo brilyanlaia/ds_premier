310c8efb617123e8cf62aa52e0c38a025ee2f53c
brilyanlaia <brilyan.laia@mhealth.tech>
2022-08-17 23:06:36 +0700

    initial commit

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-card class="rounded-0">
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Dashboard</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        bottom
+        app
+      >
+        <!-- <v-list
+           :items="items"
+        ></v-list> -->
+        <v-list>
+            <v-list-item
+          v-for="i in items"
+          :to="i.value"
+          :key="i"
+          link
+          active-class="active-link"
+          class="rounded"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ i.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Home',
+          value: '/dashboard',
+        },
+        {
+          title: 'Dummy',
+          value: '/dashboard/dummy',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
+
+<style>
+ .v-main{
+    background: #f2f3f7 !important;
+    height: 100vh;
+  }
+  .v-app-bar.v-toolbar{
+     background: #111827 !important;
+     color: #f2f3f7 !important;
+  }
+  .v-navigation-drawer{
+    background: #111827 !important;
+    color: white !important;
+  }
+  .active-link{
+    background-color: #2196f321 !important;
+    color: #1e89de !important;
+  }
+</style>
---
+++ src/views/ListView.vue
@@ -0,0 +1,91 @@
+<template>
+
+  <v-container>
+      <v-row justify="center">
+          <v-card class="card-table" elevation="4">
+            <v-card-title>Table Title </v-card-title>
+
+              <v-table>
+                <thead>
+                  <tr>
+                    <th class="text-left">
+                      Name
+                    </th>
+                    <th class="text-left">
+                      Calories
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="item in desserts"
+                    :key="item.name"
+                  >
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.calories }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
+          </v-card>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+.card-table{
+    width: 800px;
+    padding: 20px;
+    margin-top: 40px;
+}
+</style>
